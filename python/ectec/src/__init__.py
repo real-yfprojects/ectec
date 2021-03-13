be2bbb59b510d08a5f39fb66e9a24fa88c048cc6
real-yfprojects <real-yfprojects@users.noreply.github.com>
2021-03-13 16:39:52 +0100

    Define Client API in Python
    * Defines a very first client API in Python code.
    
    * Adds a module `version` for managing version numbers.

---
+++ python/ectec/src/__init__.py
@@ -0,0 +1,358 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+The core package of the ectec chattool.
+
+The package provides all the lower level functionalities that can be used
+by a GUI or other applications.
+
+
+***********************************
+
+Created on Wed Feb 17 12:13:03 2021
+
+Copyright (C) 2020 real-yfprojects (github.com user)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
+from typing import Callable, List, Union
+
+from .version import SemanticVersion, Version
+
+VERSION: Version = SemanticVersion(0, 0, 1)
+
+
+# ---- Exceptions
+
+class EctecException(Exception):
+    """
+    Superclass of all Exceptions defined in the ectec API.
+    """
+
+
+class ConnectException(EctecException):
+    """
+    Raised if there is an error on the process of connecting to a server.
+    """
+
+
+class IncompatibleServer(ConnectException):
+    """
+    The server is not understandable using the used ectec protocoll.
+    """
+
+
+class IncompatibleVersion(ConnectException):
+    """
+    The client and the server have incompatible version numbers.
+    """
+
+# ---- Assets
+
+
+class Package():
+    """
+    A package being sent using ectec.
+
+    This class can be used before and after sending.
+
+    Parameters
+    ----------
+    sender : str
+        the user who sends the package.
+    recipient : str
+        the user this package is sent to.
+    type : str
+        the content type.
+    time : float, optional
+        the time the package was received. The default is None.
+
+    Attributes
+    ----------
+    sender : TYPE
+        the user who sends the package.
+    recipient : TYPE
+        the user this package is sent to.
+    type : TYPE
+        the content type.
+    content : bytes or TYPE
+        the body of the package.
+    time : TYPE
+        the time the package was received. Might be None.
+
+    """
+
+    def __init__(self, sender: str, recipient: str, type: str,
+                 time : float = None):
+        """
+
+        A package being sent using ectec.
+
+        This class can be used before and after sending.
+
+        Parameters
+        ----------
+        sender : str
+            the user who sends the package.
+        recipient : str
+            the user this package is sent to.
+        type : str
+            the content type.
+        time : float, optional
+            the time the package was received. The default is None.
+
+        Attributes
+        ----------
+        sender : TYPE
+            the user who sends the package.
+        recipient : TYPE
+            the user this package is sent to.
+        type : TYPE
+            the content type.
+        content : bytes or TYPE
+            the body of the package.
+        time : float
+            the time the package was received. Might be None.
+
+
+        """
+
+    @property
+    def content(self):
+        """Property content"""
+
+        raise NotImplementedError("Must be implemented by subclasses")
+
+
+# ---- Client API / Standard User
+
+class PackageStorage:
+    """
+    Stores and manages packages.
+
+
+    """
+
+    def __init__(self):
+        pass
+
+    def remove(self, *packages : List[Package],
+               func: Callable[[Package], bool] = None) -> int :
+        """
+        Remove packages from the storage.
+
+        If `packages` is specified this method returns the number of
+        packages in `packages` that could not be removed.
+
+        Parameters
+        ----------
+        *packages : Package (optional)
+            Packages to be removed.
+        func : callable(Package) -> bool, optional
+            A function acting as a filter.
+
+        Returns
+        -------
+        Int.
+
+        """
+        return 0
+
+    def add(self, *packages : List[Package]):
+        """
+        Add packages to the PackageStorage.
+
+        Parameters
+        ----------
+        *packages : Package
+            The packages.
+
+        Returns
+        -------
+        None.
+
+        """
+
+    def all(self) -> List[Package] :
+        """
+        Return a list of all packages in the PackageStorage.
+
+        If not modified the packages are ordered in the order they were added
+        to the PackageStorage.
+
+        Returns
+        -------
+        List of Package.
+
+        """
+
+        return []
+
+    def filter(self, func : Callable[[Package], bool]=None,
+               **kwargs) -> List[Package] :
+        """
+        Return a filtered list of the packages in the PackageStorage.
+
+        Parameters
+        ----------
+        func : callable(Package) -> bool, optional
+            A function acting as a filter.
+        **kwargs : Any.
+            Attributes of Package that should match.
+
+        Returns
+        -------
+        list of Package.
+
+        """
+        return []
+
+
+class UserClient:
+    """
+    A Client for the normal user role.
+
+    Parameters
+    ----------
+    username : str
+        The name for this client which is used as an identifier.
+
+    Attributes
+    ----------
+    version : Version
+        Equals `VERSION` of this python module.
+    username : str
+        The name for this client which is used as an identifier.
+    users : list of str
+        list of users connected to the server.
+    packages : PackageStorage
+        The PackageStorage managing the received packages.
+
+    """
+
+    version = VERSION
+
+    def __init__(self, username : str):
+        """
+        A Client for the normal user role.
+
+        Parameters
+        ----------
+        username : str
+            The name for this client which is used as an identifier.
+
+        Attributes
+        ----------
+        version : Version
+            Equals `VERSION` of this python module.
+        username : str
+            The name for this client which is used as an identifier.
+        users : list of str
+            list of users connected to the server.
+        packages : PackageStorage
+            The PackageStorage managing the received packages.
+
+        """
+        self.username : str = username
+        self.users : List[str]
+        self.packages : PackageStorage
+
+    def connect(self, server : str, port : int):
+        """
+        Connect to a server.
+
+        Parameters
+        ----------
+        server : str
+            The ip or hostname.
+        port : int
+            The port to connect to.
+
+        Raises
+        ------
+        ConnectException
+            The connection couldn't be established.
+
+        Returns
+        -------
+        None.
+
+        """
+        raise NotImplementedError("Must be implemented by subclasses")
+
+    def disconnect(self):
+        """
+        Disconnect from the server the client currently is connected to.
+
+        This then allows connecting to a different server.
+
+        Returns
+        -------
+        None.
+
+        """
+
+    def send(self, package : Package):
+        """
+        Send a package.
+
+        Parameters
+        ----------
+        package : Package
+            The package.
+
+        Raises
+        ------
+        Exceptions related to sending if the package couldn't be sent.
+
+        Returns
+        -------
+        None.
+
+        """
+        raise NotImplementedError("Must be implemented by subclasses")
+
+    def receive(self, n : int = None) -> Union[Package, List[Package]]:
+        """
+        Read out the buffer of Packages.
+
+        The buffer contains all Packages that were received since the last
+        time the buffer was read. The optional `n` specified how many packages
+        should be removed from the buffer and returned. If `n=1` a Package is
+        returned. If `n>1` a list of Packages is returned. If `n=None` or
+        `n<1` all packages in the buffer are returned.
+
+        Parameters
+        ----------
+        n : positive int, optional
+            Number of packages to read out. The default is None.
+
+        Returns
+        -------
+        Package or list of Package.
+
+        """
+        raise NotImplementedError("Must be implemented by subclasses")
+
+    @property
+    def server(self):
+        """
+        Property `server`. Read only.
+
+        """
+        raise NotImplementedError("Must be implemented by subclasses")
+
---
+++ python/ectec/src/version.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Versioning in Python.
+
+@version: 0.0
+
+
+***********************************
+
+Created on Fri Feb 26 16:49:40 2021
+
+Copyright (C) 2020 real-yfprojects (github.com user)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
+from functools import total_ordering
+
+
+class VersionException(Exception):
+    """
+    Superclass of all Exceptions related to versions
+    """
+
+class Version:
+    """
+    The superclass of all Versions.
+
+    This class is intended for type annotations
+    """
+
+
+class SemanticVersionException(VersionException):
+    """
+    Exception related to semantic version numbers
+    """
+
+
+class SubVersionException(SemanticVersionException):
+    """
+    Raise if there are too many subversions in a semantic version.
+    """
+
+@total_ordering
+class SemanticVersion(Version):
+    """
+    Implementation of a Semantic Version Number.
+
+    Notes
+    -----
+
+    Given a version number MAJOR.MINOR.PATCH, increment the:
+
+    MAJOR version when you make incompatible API changes,
+    MINOR version when you add functionality in a backwards compatible manner,
+    PATCH version when you make backwards compatible bug fixes.
+
+    Additional labels for pre-release and build metadata are available as
+    extension to the MAJOR.MINOR.PATCH format.
+
+    Labels for pre-release versions may be appended using a hyphen `-`.
+    Build metadata may be denoted by appending it after a plussign `+`.
+
+    """
+
+    def __init__(self, major, minor, patch, label=''):
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+        self.label = label
+        self.metadata = ''
+
+    def add_metadata(self, metadata):
+        self.metadata = metadata
+
+    def __repr__(self):
+        return '<SemanticVersion \'{}\' >'.format(str(self))
+
+    def __str__(self):
+        main = str(self.major) + '.' + str(self.minor) + '.' + str(self.patch)
+        label = '-' + str(self.label) if self.label else ''
+        metadata = '+' + str(self.metadata) if self.metadata else ''
+        return main + label + metadata
+
+    def __gt__(self, other):
+        a = self.major == other.major
+        b = self.minor == other.minor
+        c = self.major > other.major
+        d = self.minor > other.minor
+        e = self.patch > other.patch
+        l = not (self.label or other.label)
+        return c or (a and (d or (b and e and l)))
+
+    def __eq__(self, other):
+        a = self.major == other.major
+        b = self.minor == other.minor
+        c = self.patch == other.patch
+        d = self.label == other.label
+        return a and b and c and d
+
+    def equals(self, other):
+        """
+        Test wether two SemanticVersion are identical.
+
+        Parameters
+        ----------
+        other : SemanticVersion
+            the other version number.
+
+        Returns
+        -------
+        boolean
+            the return value.
+
+        """
+        a = self.major == other.major
+        b = self.minor == other.minor
+        c = self.patch == other.patch
+        d = self.label == other.label
+        e = self.metadata == other.metadata
+        return a and b and c and d and e
+
+    @classmethod
+    def parse(cls, string: str):
+        """
+        Parses a string describing a semantic version number.
+
+        It creates a SemanticVersion objekt representing
+        the same version number as the string.
+
+        Parameters
+        ----------
+        string : str
+            DESCRIPTION.
+
+        Raises
+        ------
+        SubVersionException
+            In case of too many subversion numbers.
+
+        Returns
+        -------
+        SemanticVersion
+            The objekt.
+
+        """
+
+        string = string.strip()
+        major = ''
+        minor = ''
+        patch = ''
+        label = ''
+        metadata = ''
+
+        seq = 0
+        for s in string:
+            if seq == 0:
+                # major
+                if s == '.':
+                    seq += 1
+                elif s == '-':
+                    seq = 3
+                elif s == '+':
+                    seq = 4
+                elif s.isdigit():
+                    major += s
+
+            elif seq == 1:
+                # minor
+                if s == '.':
+                    seq += 1
+                elif s == '-':
+                    seq = 3
+                elif s == '+':
+                    seq = 4
+                elif s.isdigit():
+                    minor += s
+
+            elif seq == 2:
+                # patch
+                if s == '.':
+                    raise SubVersionException('More than 3 subversions')
+
+                if s == '-':
+                    seq = 3
+                elif s == '+':
+                    seq = 4
+                elif s.isdigit():
+                    patch += s
+
+            elif seq == 3:
+                # label
+                if s == '+':
+                    seq = 4
+                elif not s.isspace():
+                    label += s
+
+            else:
+                if not s.isspace():
+                    metadata += s
+
+        major = int(major) if major else 0
+        minor = int(minor) if minor else 0
+        patch = int(patch) if patch else 0
+
+        objekt = cls(major, minor, patch, label)
+        objekt.add_metadata(metadata)
+
+        return objekt
